ee3d377e18085c3c01f3bfd9dde529b92c0e0b4f
tsenoner <tobias.senoner94@gmail.com>
2025-11-28 14:52:51 +0100

    refactor(potenci): clean up module exports
    - Export modern API: load_central_shifts, PHYSICAL_CONSTANTS, etc.
    - Remove legacy exports: R, a, b, cutoff, e, ncycles
    - Add module docstring
    - Update __all__ for clean public API

--- trizod/potenci/__init__.py
+++ trizod/potenci/__init__.py
@@ -1,2 +1,27 @@
-from .potenci import getpredshifts
-from .constants import R, e, a, b, cutoff, ncycles
+"""POTENCI module for predicting random coil NMR chemical shifts."""
+
+from .constants import AA_STANDARD as AA_STANDARD
+from .constants import PHYSICAL_CONSTANTS as PHYSICAL_CONSTANTS
+from .constants import PK0 as PK0
+from .constants import PhysicalConstants as PhysicalConstants
+from .constants import load_central_shifts as load_central_shifts
+from .constants import load_combinatorial_deviations as load_combinatorial_deviations
+from .constants import load_neighbor_corrections as load_neighbor_corrections
+from .constants import load_ph_shifts as load_ph_shifts
+from .constants import load_temperature_coefficients as load_temperature_coefficients
+from .constants import load_terminal_corrections as load_terminal_corrections
+from .potenci import getpredshifts as getpredshifts
+
+__all__ = [
+    "AA_STANDARD",
+    "PHYSICAL_CONSTANTS",
+    "PK0",
+    "PhysicalConstants",
+    "load_central_shifts",
+    "load_combinatorial_deviations",
+    "load_neighbor_corrections",
+    "load_ph_shifts",
+    "load_temperature_coefficients",
+    "load_terminal_corrections",
+    "getpredshifts",
+]
